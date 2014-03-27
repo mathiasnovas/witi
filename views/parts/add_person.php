eacbf89e6603e12279a64622d0f632ae4d77370a
Tobias Føyn Føyen <tobias.foyen@gmail.com>
2014-03-27 22:55:42 +0100

    Form and lightbox stuff

--- views/parts/add_person.php
+++ views/parts/add_person.php
@@ -1,14 +1,17 @@
 <form class="add-person" role="form" action="bin/add.php" type="">
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input class="form-control" type="text" id="name" name="name" placeholder="Name">
-    </div>
-    
-    <div class="form-group">
-        <label for="image">Image</label>
-        <input type="file" accept="image/*;capture=camera" name="image[]" id="image">
-    </div>
+	<div class="wrapper">
+		<h1>Add new person</h1>
+	    <div class="form-group">
+	        <label for="name">Name</label>
+	        <input class="form-control" type="text" id="name" name="name" placeholder="Name">
+	    </div>
+	    
+	    <div class="form-group">
+	        <label for="image">Image</label>
+	        <input type="file" accept="image/*;capture=camera" name="image[]" id="image">
+	    </div>
 
-    <input type="hidden" name="type" value="person">
-    <button class="btn btn-default" type="submit">Go</button>
+	    <input type="hidden" name="type" value="person">
+	    <button class="btn btn-default" type="submit">Go</button>
+	</div>
 </form>
