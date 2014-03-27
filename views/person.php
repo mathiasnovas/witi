a6e06ccfb101babd45fab4db7ef86ecf76077933
Mathias <mathias_novas@hotmail.com>
2014-03-27 13:21:34 +0100

    ditch bower, yo

--- views/person.php
+++ views/person.php
@@ -2,17 +2,16 @@
 
 $id = witi::parseUrl('id');
 $person = witi::fetchPerson($id);
-
-$gadgets = witi::fetchGadgets($person['id']);
+$gadgets = witi::fetchGadgetsById($id);
 
 ?>
 
 <article class="person">
-
     <h1><?php print $person['name']; ?></h1>
 
-    <ul class="data">
-        <li><?php print $gadget['name']; ?></li>
+    <ul class="gadgets">
+        <?php foreach ($gadgets as $gadget) { ?>
+            <li><?php print $gadget['name']; ?></li>
+        <?php } ?>
     </ul>
-
 </article>
